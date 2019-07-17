690a10db95be417bdabf0d887296d0ad3c0af9a1
Marco Porcho <maporcho@gmail.com>
2019-07-16 21:37:18 -0300

    Creating Custom Flutter Widgets

---
+++ lib/input_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'labeled_icon.dart';
+import 'reusable_card.dart';
+
+const bottomContainerHeight = 80.0;
+const cardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    color: cardColor,
+                    cardChild: LabeledIcon(
+                      iconData: FontAwesomeIcons.mars,
+                      text: 'MALE',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                      color: cardColor,
+                      cardChild: LabeledIcon(
+                        iconData: FontAwesomeIcons.venus,
+                        text: 'FEMALE',
+                      )),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(color: cardColor),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(color: cardColor),
+                ),
+                Expanded(
+                  child: ReusableCard(color: cardColor),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
+        ],
+      ),
+    );
+  }
+}
