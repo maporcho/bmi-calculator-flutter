72213d23753ba4436a388a229a6c840c774bd0cb
Marco Porcho <maporcho@gmail.com>
2019-07-21 12:56:28 -0300

    Composition vs. Inheritance - Building Flutter Widgets From Scratch

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,6 +7,10 @@ const kBottomContainerColor = Color(0xFFEB1555);
 
 const kMinHeight = 120.0;
 const kMaxHeight = 220.0;
+const kMinWeight = 0;
+const kMaxWeight = 200;
+const kMinAgeInYears = 10;
+const kMaxAgeInYears = 150;
 
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -15,6 +15,8 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   Gender selectedGender;
   int height = 180;
+  int weight = 60;
+  int years = 25;
 
   @override
   Widget build(BuildContext context) {
@@ -89,17 +91,29 @@ class _InputPageState extends State<InputPage> {
                             ),
                           ],
                         ),
-                        Slider(
-                          activeColor: Color(0xFFEB1555),
-                          inactiveColor: Color(0XFF8D8E98),
-                          value: height.toDouble(),
-                          min: kMinHeight,
-                          max: kMaxHeight,
-                          onChanged: (double newValue) {
-                            setState(() {
-                              this.height = newValue.round();
-                            });
-                          },
+                        SliderTheme(
+                          data: SliderTheme.of(context).copyWith(
+                            activeTrackColor: Colors.white,
+                            inactiveTrackColor: Color(0XFF8D8E98),
+                            thumbColor: Color(0xFFEB1555),
+                            overlayColor: Color(0x29EB1555),
+                            thumbShape: RoundSliderThumbShape(
+                              enabledThumbRadius: 15.0,
+                            ),
+                            overlayShape: RoundSliderOverlayShape(
+                              overlayRadius: 30.0,
+                            ),
+                          ),
+                          child: Slider(
+                            value: height.toDouble(),
+                            min: kMinHeight,
+                            max: kMaxHeight,
+                            onChanged: (double newValue) {
+                              setState(() {
+                                this.height = newValue.round();
+                              });
+                            },
+                          ),
                         )
                       ],
                     ),
@@ -112,10 +126,112 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(color: kActiveCardColor),
+                  child: ReusableCard(
+                    color: kActiveCardColor,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'WEIGHT',
+                          style: kLabelTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.alphabetic,
+                          children: <Widget>[
+                            Text(
+                              weight.toString(),
+                              style: kNumberTextStyle,
+                            ),
+                            Text(
+                              "kg",
+                              style: kLabelTextStyle,
+                            )
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RoundIconButton(
+                              icon: FontAwesomeIcons.minus,
+                              onTap: () {
+                                setState(() {
+                                  this.weight = this.weight > kMinWeight
+                                      ? this.weight - 1
+                                      : this.weight;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            RoundIconButton(
+                                icon: FontAwesomeIcons.plus,
+                                onTap: () {
+                                  setState(() {
+                                    this.weight = this.weight < kMaxWeight
+                                        ? this.weight + 1
+                                        : this.weight;
+                                  });
+                                }),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ),
                 Expanded(
-                  child: ReusableCard(color: kActiveCardColor),
+                  child: ReusableCard(
+                    color: kActiveCardColor,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'AGE',
+                          style: kLabelTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.alphabetic,
+                          children: <Widget>[
+                            Text(
+                              years.toString(),
+                              style: kNumberTextStyle,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RoundIconButton(
+                              icon: FontAwesomeIcons.minus,
+                              onTap: () {
+                                setState(() {
+                                  this.years = this.years > kMinAgeInYears
+                                      ? this.years - 1
+                                      : this.years;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            RoundIconButton(
+                                icon: FontAwesomeIcons.plus,
+                                onTap: () {
+                                  setState(() {
+                                    this.years = this.years < kMaxAgeInYears
+                                        ? this.years + 1
+                                        : this.years;
+                                  });
+                                }),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -128,6 +244,28 @@ class _InputPageState extends State<InputPage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class RoundIconButton extends StatelessWidget {
+  final IconData icon;
+  final Function onTap;
+
+  RoundIconButton({this.icon, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      elevation: 6.0,
+      onPressed: onTap,
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
     );
   }
 }
